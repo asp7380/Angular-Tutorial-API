36b89d51b79d34fa44d30ce7312705c5a6533a46
Adrian Parkhill <adrpar7380@gmail.com>
2021-01-29 03:26:50 -0500

    Initial commit of Test API

---
+++ models/PaymentDetail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentsAPI.models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Column(TypeName = "Nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+
+        [Column(TypeName = "Nvarchar(16)")]
+        public string CardNumber { get; set; }
+
+        [Column(TypeName = "Nvarchar(5)")]
+        public string ExpirationDate { get; set; }
+
+        [Column(TypeName = "Nvarchar(3)")]
+        public string SecurityCode { get; set; }
+
+
+    }
+}
